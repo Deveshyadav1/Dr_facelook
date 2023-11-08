72b62934796097223285ccc3b39a4eac4383d9fc
Deveshyadav1 <50092905+Deveshyadav1@users.noreply.github.com>
2023-11-09 01:16:39 +0530

    Chatify module added

--- resources/views/doctor/home.blade.php
+++ resources/views/doctor/home.blade.php
@@ -75,7 +75,7 @@
                     <a href="/appointment" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Appointment</a>
 
 
-                    <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
+                    <a href="/chatify" class="nav-item nav-link"><i class="fas fa-comment-dots"></i>Chat With Doctor</a>
                     <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
                     <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
                     <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
